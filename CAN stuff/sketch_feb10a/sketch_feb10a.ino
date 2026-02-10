3d80234d37508d093a4dde3cebf5d5f621bbd969
Yam <yanjason435@gmail.com>
2026-02-10 17:01:47 -0500

    Update sketch_feb10a.ino

--- CAN stuff/sketch_feb10a/sketch_feb10a.ino
+++ CAN stuff/sketch_feb10a/sketch_feb10a.ino
@@ -2,3 +2,21 @@
 #include <mcp_can.h>
 
 #define CAN_CS 10
+
+MCP_CAN CAN(CAN_CS);
+
+
+void setup() {
+  while (1){
+    Serial.begin(9600);
+
+    while (CAN_OK != CAN.begin(MCP_ANY, CAN_500KBPS, MCP_16MHZ)) {
+      Serial.println("CAN init failed, retrying...");
+      delay(100);
+    }
+
+    Serial.println("CAN init OK!");
+    CAN.setMode(MCP_NORMAL);
+  }
+
+}
